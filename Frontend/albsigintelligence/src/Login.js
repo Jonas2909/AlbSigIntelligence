e1d75441fcb6cc8473f6a7409f779fd21fa713ad
Jonas2909 <jonas.reinecke@live.de>
2023-11-15 16:15:36 +0100

    Edited Login Page

--- Frontend/albsigintelligence/src/Login.js
+++ Frontend/albsigintelligence/src/Login.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-      <h2>Login Component</h2>
+      <h2>Login Seite</h2>
       {/* Add your login form or content here */}
     </div>
   );
