acdda55290a133ee3249f82ebc3c058713a0463c
Jonas2909 <jonas.reinecke@live.de>
2023-11-15 15:40:05 +0100

    Created React Project for Frontend and implemented initialzing

---
+++ Frontend/albsigintelligence/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ Frontend/albsigintelligence/src/Login.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login Component</h2>
+      {/* Add your login form or content here */}
+    </div>
+  );
+};
+
+export default Login;
