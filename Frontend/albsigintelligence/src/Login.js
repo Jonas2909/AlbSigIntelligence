22f1cd00e504ddd343e113f75aba56a2dfef419d
Jonas2909 <jonas.reinecke@live.de>
2023-11-18 15:19:39 +0100

    Edited Login UI and added Registration opo-up

--- Frontend/albsigintelligence/src/Login.js
+++ Frontend/albsigintelligence/src/Login.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -12,13 +10,15 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://www.hs-albsig.de/">
+        AlbSigIntelligence
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -26,47 +26,70 @@ function Copyright(props) {
   );
 }
 
-// TODO remove, this demo shouldn't need to reset the theme.
-
 const defaultTheme = createTheme();
 
 export default function SignIn() {
+  const [openSignupDialog, setOpenSignupDialog] = React.useState(false);
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      emailinput: data.get('username'),
+      passwordinput: data.get('password'),
     });
   };
+
+  const handleSignupSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      field1: data.get('field1'),
+      field2: data.get('field2'),
+      field3: data.get('field3'),
+      field4: data.get('field4'),
+    });
+    handleSignupClose();
+  };
+
+  const handleSignupClick = () => {
+    setOpenSignupDialog(true);
+  };
+
+  const handleSignupClose = () => {
+    setOpenSignupDialog(false);
+  };  
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+        <CssBaseline />
         <Box
           sx={{
+            border: `4px solid ${defaultTheme.palette.primary.main}`,
+            borderRadius: 8,
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            padding: 3,
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            Dear User, Thank you for using our services. To access your account, please enter your user credentials.
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
               autoFocus
             />
             <TextField
@@ -79,33 +102,76 @@ export default function SignIn() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Login
+            </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSignupClick}
+            >
+              Sign-Up
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
+
+        <Dialog open={openSignupDialog} onClose={handleSignupClose}>
+          <DialogContent>
+            <form onSubmit={handleSignupSubmit}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="firstname"
+                label="Firstname"
+                name="firstname"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="lastname"
+                label="Lastname"
+                name="lastname"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                name="password"
+                type='password'
+              />
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
+                Submit
+              </Button>
+            </form>
+          </DialogContent>
+        </Dialog>
+
       </Container>
     </ThemeProvider>
   );
