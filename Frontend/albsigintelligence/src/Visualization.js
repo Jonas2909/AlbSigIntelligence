45cf23ad3a1b24deb6e3ade69eddd754bdebe254
Jonas2909 <jonas.reinecke@live.de>
2023-11-21 18:59:47 +0100

    Added Pop-Up Message which appears if user is not logged in

--- Frontend/albsigintelligence/src/Visualization.js
+++ Frontend/albsigintelligence/src/Visualization.js
@@ -1,20 +1,37 @@
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@material-ui/core';
 
 export default function Visualization() {
     const navigate = useNavigate();
+    const [isDialogOpen, setDialogOpen] = useState(false);
 
     useEffect(() => {
-        // Check session status
-        const isLoggedIn = sessionStorage.getItem('isLoggedIn');
-        if (!isLoggedIn) {
-          // Redirect to login if not logged in
-          console.log("User not logged in!");
-          navigate('/');
-        }
-      }, [navigate]);
+      const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+      if (!isLoggedIn) {
+        setDialogOpen(true);
+      }
+    }, []);
 
-  return (
-    <h1>Visualization</h1>
-  );
-}
+    const handleDialogClose = () => {
+      setDialogOpen(false);
+      navigate('/');
+    };
+
+    return (
+      <div>
+        <h1>Visualization</h1>
+        <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+          <DialogTitle>Error</DialogTitle>
+          <DialogContent>
+            <p>User not logged in!</p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose} color="primary">
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
