3af829e25a4a2d169fc5f9dc40d024e7571cc7d5
Jonas2909 <jonas.reinecke@live.de>
2023-11-29 09:55:32 +0100

    Added HTTP encryption to Rest-Service an generated self signed certificates

--- Test-DockerCompose/RestService.py
+++ Test-DockerCompose/RestService.py
@@ -61,4 +61,4 @@ def delete_user():
         return "Invalid data. 'username' is required in the request."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=5000, debug=True)
