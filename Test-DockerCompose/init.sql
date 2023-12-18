fb20115b10912418a152fa0906219642f8f4e8a1
Jonas2909 <jonas.reinecke@live.de>
2023-12-18 17:15:51 +0100

    Added Table mac_adresses to init.sql file

--- Test-DockerCompose/init.sql
+++ Test-DockerCompose/init.sql
@@ -13,11 +13,21 @@ CREATE TABLE IF NOT EXISTS measurements (
 
 );
 
+CREATE TABLE IF NOT EXISTS mac_adresses (
+    id serial PRIMARY KEY,
+    mac VARCHAR(100) NOT NULL,
+    time_stamp bigint NOT NULL
+);
+
 INSERT INTO user_credentials (firstname,lastname, username, password) VALUES ('samplefirstname', 'samplelastname', 'sampleuser', 'samplepassword');
 INSERT INTO user_credentials (firstname,lastname, username, password) VALUES ('jonas', 'reinecke', 'username', 'password');
 
 INSERT INTO measurements (time_stamp, quantity) VALUES (1701174250, 25);
 INSERT INTO measurements (time_stamp, quantity) VALUES (1701176250, 15);
 
+INSERT INTO mac_adresses (mac, time_stamp) VALUES ('00-1D-60-4A-8C-CB', 1702915589);
+INSERT INTO mac_adresses (mac, time_stamp) VALUES ('01-1D-60-4A-8C-CB',  1703174789);
+
 GRANT ALL ON user_credentials TO postgres;
 GRANT ALL ON measurements TO postgres;
+GRANT ALL ON mac_adresses TO postgres;
