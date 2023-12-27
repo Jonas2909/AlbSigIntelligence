4bb83837752f7f806e231354b1f7be355c900d8f
Jonas2909 <jonas.reinecke@live.de>
2023-12-27 17:02:57 +0100

    Cleaned up repo

--- Test-DockerCompose/init.sql
+++ Test-DockerCompose/init.sql
@@ -31,8 +31,8 @@ INSERT INTO user_credentials (firstname,lastname, username, password) VALUES ('j
 INSERT INTO measurements (time_stamp, quantity) VALUES (1701174250, 25);
 INSERT INTO measurements (time_stamp, quantity) VALUES (1701176250, 15);
 
-INSERT INTO mac_addresses (mac, time_stamp) VALUES ('00-1D-60-4A-8C-CB', 1702915589);
-INSERT INTO mac_addresses (mac, time_stamp) VALUES ('01-1D-60-4A-8C-CB',  1703174789);
+INSERT INTO mac_addresses (mac, time_stamp) VALUES ('00-1D-60-4A-8C-CB', 1703696400);
+INSERT INTO mac_addresses (mac, time_stamp) VALUES ('01-1D-60-4A-8C-CB',  1703523600);
 
 GRANT ALL ON user_credentials TO postgres;
 GRANT ALL ON measurements TO postgres;
